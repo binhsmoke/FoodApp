02d6693255c8d8a893263ad0013e950fdc5e08d8
luanntps <99125936+luanntps@users.noreply.github.com>
2022-07-08 16:56:16 +0700

    2nd

---
+++ app/src/main/java/com/example/baemin/Activity/StartActivity.java
@@ -0,0 +1,26 @@
+package com.example.baemin.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.baemin.R;
+
+public class StartActivity extends AppCompatActivity {
+    LinearLayout buttonStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        buttonStart=findViewById(R.id.llStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
