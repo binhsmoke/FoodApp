d35c0d1ac3e41f4a702fbdc99aed6d9dc4116ed2
binhsmoke <binhsmoke@gmail.com>
2022-07-25 08:36:47 +0700

    client
    layout client

---
+++ app/src/main/java/com/example/baemin/Activity/ClientActivity.java
@@ -0,0 +1,16 @@
+package com.example.baemin.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.baemin.R;
+
+public class ClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+    }
+}
